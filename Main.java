4ff57b5ca1a90fcba2b7cbb32092877972ef9a70
eoroose <eric.roose15@gmail.com>
2020-04-13 20:42:01 +0200

    lwjgl update

--- Main.java
+++ Main.java
@@ -1,7 +1,123 @@
+import java.nio.IntBuffer;
+
+//import Example.Example;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.glfw.Callbacks;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+//import pixelEngine.PixelEngine;
+
+import static org.lwjgl.opengles.GLES20.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengles.GLES20.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
 public class Main {
 
+    private long window;
+    private final int WIDTH = 640, HEIGHT= 480;
+    //private PixelEngine pixelEngine;
+    //private Example example;
+
+    public void run() {
+        setup();
+        loop();
+
+        Callbacks.glfwFreeCallbacks(window);
+        GLFW.glfwDestroyWindow(window);
+
+        GLFW.glfwTerminate();
+        GLFW.glfwSetErrorCallback(null).free();
+    }
+
+    public void setup() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if(!GLFW.glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        GLFW.glfwDefaultWindowHints();
+        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_FALSE);
+
+        window = GLFW.glfwCreateWindow(WIDTH, HEIGHT, "NES Emulator", NULL, NULL);
+        if(window == NULL) {
+            throw new IllegalStateException("Unable to create GLFW Window");
+        }
+
+        GLFW.glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {});
+
+        try(MemoryStack stack = stackPush()){
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+
+            GLFW.glfwGetWindowSize(window, pWidth, pHeight);
+
+            GLFWVidMode vidmode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+
+            GLFW.glfwSetWindowPos(window,(vidmode.width() - pWidth.get(0)) / 2,(vidmode.height() - pHeight.get(0)) / 2);
+
+            GLFW.glfwMakeContextCurrent(window);
+            GLFW.glfwSwapInterval(1);
+            GLFW.glfwShowWindow(window);
+        }
+    }
+
+    public void loop() {
+        GL.createCapabilities();
+        init();
+
+        int fps = 60;
+        int ticks = 0;
+        double delta = 0;
+        double timePerTick = 1000000000.0 / fps;
+        long now;
+        long timer = 0;
+        long lastTime = System.nanoTime();
+
+        while(!GLFW.glfwWindowShouldClose(window)) {
+
+            now = System.nanoTime();
+            delta += (now - lastTime) / timePerTick;
+            timer += now - lastTime;
+            lastTime = now;
+
+            if (delta >= 1) {
+                tick();
+                render();
+                ticks++;
+                delta--;
+            }
+            if (timer >= 1000000000) {
+                System.out.println("fps: " + ticks);
+                ticks = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    private void init() {
+        //pixelEngine = new PixelEngine(WIDTH, HEIGHT);
+        //example = new Example(pixelEngine);
+    }
+
+    private void tick() {
+        //pixelEngine.tick();
+        //example.tick();
+    }
+
+    private void render() {
+        GL11.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        //pixelEngine.render();
+        GLFW.glfwSwapBuffers(window);
+        GLFW.glfwPollEvents();
+    }
+
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello world!");
+        new Main().run();
     }
 }
